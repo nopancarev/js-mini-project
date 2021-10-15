12fb7d5a6b3353c5657b7ae8f990157de0abebb8
Nenad Opancarev <nopancarev@gmail.com>
2021-10-15 13:46:13 +0200

    js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-var jsView = document.querySelector('#jsView');
-var phpView = document.querySelector('#phpView');
-var wpView = document.querySelector('#wpView');
-var jsViewImg = document.querySelector('#jsViewImg');
-var phpViewImg = document.querySelector('#phpViewImg');
-var wpViewImg = document.querySelector('#wpViewImg');
+var jsView = document.querySelector("#jsView");
+var phpView = document.querySelector("#phpView");
+var wpView = document.querySelector("#wpView");
+var jsViewImg = document.querySelector("#jsViewImg");
+var phpViewImg = document.querySelector("#phpViewImg");
+var wpViewImg = document.querySelector("#wpViewImg");
 
 function showJsView() {
   jsView.style.display = "block";
